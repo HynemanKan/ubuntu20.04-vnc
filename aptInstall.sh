807e977e5bf913cafa9c8598948a531e84198465
HynemanKan <33802829+HynemanKan@users.noreply.github.com>
2022-11-08 09:19:03 +0800

    Create aptInstall.sh

---
+++ aptInstall.sh
@@ -0,0 +1,9 @@
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+echo "deb http://mirrors.ustc.edu.cn/ubuntu/ focal main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb http://mirrors.ustc.edu.cn/ubuntu/ focal-updates main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb http://mirrors.ustc.edu.cn/ubuntu/ focal-backports main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb http://mirrors.ustc.edu.cn/ubuntu/ focal-security main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb http://mirrors.ustc.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse" >> /etc/apt/sources.list
+apt-get update
+apt install tasksel -y
+apt install ubuntu-desktop -y
